3454e4cea45833d8c809f3184581cc6508d805c6
Uwaraba913 <metalcoola5963@gmail.com>
2021-08-23 02:53:06 +0000

    finish

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,11 +1,14 @@
 class BooksController < ApplicationController
+  before_action :check_user, only: [:edit, :update, :destroy]
 
   def show
     @book = Book.find(params[:id])
+    @new_book = Book.new
   end
 
   def index
     @books = Book.all
+    @book = Book.new
   end
 
   def create
@@ -15,7 +18,7 @@ class BooksController < ApplicationController
       redirect_to book_path(@book), notice: "You have created book successfully."
     else
       @books = Book.all
-      render 'index'
+      render :index
     end
   end
 
@@ -30,20 +33,26 @@ class BooksController < ApplicationController
     if @book.update(book_params)
       redirect_to book_path(@book), notice: "You have updated book successfully."
     else
-      render edit_book_path(@book)
+      render :edit
     end
   end
 
-  def delete
+  def destroy
     @book = Book.find(params[:id])
-    @book.destoy
+    @book.delete
     redirect_to books_path
   end
 
   private
 
   def book_params
-    params.permit(:title, :body)
+    params.require(:book).permit(:title, :body)
+  end
+
+  def check_user
+    unless Book.find(params[:id]).user == current_user
+    redirect_to books_path
+    end
   end
 
 end
